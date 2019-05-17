2e9f4c6127e5f86e75432b693a789f240f7aacc4
Carlos Filoteo <c.filoteo12@gmail.com>
2019-05-17 15:47:53 -0600

    Export singleSpaReact lifecycles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import singleSpaReact from 'single-spa-react';
 
-ReactDOM.render(<App />, document.getElementById('messenger-ui'));
+import App from './App'
+
+const reactLifecycles = singleSpaReact({
+    React,
+    ReactDOM,
+    rootComponent: App,
+    domElementGetter: () => document.getElementById("messenger-ui"),
+});
+
+export const bootstrap = [
+    reactLifecycles.bootstrap,
+];
+
+export const mount = [
+    reactLifecycles.mount,
+];
+
+export const unmount = [
+    reactLifecycles.unmount,
+];

ac663541998a7597cbe6a7b6c6c823ec9ac352ec
Carlos Filoteo <c.filoteo12@gmail.com>
2019-05-20 01:07:55 -0600

    Add ConversationLink

---
+++ src/ConversationLink.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './ConversationLink.css'
+
+export default function ConversationLink(props) {
+    const { to, name, content, isReply } = props
+    return (
+        <Link to={to} className={styles.Conversation}>
+            <span className={styles.Title}>{name}</span>
+            <span className={isReply ? styles.ReplyContent : styles.ResponseContent}>{content}</span>
+        </Link>
+    )
+}

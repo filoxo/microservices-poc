9c5207a373643804c66c0d273ce69c029500f81d
Carlos Filoteo <c.filoteo12@gmail.com>
2019-05-20 01:08:18 -0600

    Add ConversationSplitContainer

---
+++ src/ConversationSplitContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './ConversationSplitContainer.css'
+
+export default function ConversationSplitContainer(props) {
+    if(React.Children.count(props.children) !== 2) {
+        console.warn("ConversationSplitContainer requires exactly two children elements.")
+        return null;
+    }
+    const [leftChild, rightChild] = React.Children.toArray(props.children)
+    return (
+        <div className={styles.Container}>
+            <div className={styles.LeftList}>{leftChild}</div>
+            {rightChild}
+        </div>
+    )
+}

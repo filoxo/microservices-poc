920615a52de2bd2b01e23190f58c3d3a67e5cb7f
Carlos Filoteo <c.filoteo12@gmail.com>
2019-05-20 01:10:02 -0600

    Replace @reach/router with react-router

--- src/Messenger.js
+++ src/Messenger.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import styles from './Messenger.css'
-import { Router } from "@reach/router"
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom"
 import AllConversations from './AllConversations'
 import Sidebar from './Sidebar'
 
-export default function Messenger(props) {
+export default function Messenger() {
     return (
-        <div className={styles.MessengerContainer}>
-            <Router>
-                <Sidebar path="conversations">
-                    <AllConversations default path="all/*"/>
-                </Sidebar>
-            </Router>
-        </div>
+        <Router>
+            <div className={styles.MessengerContainer}>
+                <Sidebar />
+                <Switch>
+                    <Route path="/conversations/all/:conversationId?" component={AllConversations}/>
+                    <Redirect exact from="/conversations" to="/conversations/all"/>
+                </Switch>
+            </div>
+        </Router>
     )
 }

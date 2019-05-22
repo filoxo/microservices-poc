e2ebca0fa81f97849dd242c8556bc75d1eea14f2
Carlos Filoteo <c.filoteo12@gmail.com>
2019-05-22 00:09:57 -0600

    Color selected ConversationLink

--- src/ConversationLink.js
+++ src/ConversationLink.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import styles from './ConversationLink.css'
 
 export default function ConversationLink(props) {
   const { to, name, content, isReply } = props
   return (
-    <Link to={to} className={styles.Conversation}>
+    <NavLink to={to} className={styles.Conversation}>
       <span className={styles.Title}>{name}</span>
       <span className={isReply ? styles.ReplyContent : styles.ResponseContent}>
         {content}
       </span>
-    </Link>
+    </NavLink>
   )
 }

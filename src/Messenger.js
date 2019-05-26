724df7e3d137f3ab260c83637e912f7f1c5ffea4
Carlos Filoteo <c.filoteo12@gmail.com>
2019-05-25 20:56:20 -0600

    Mount Message if at conversations route; add ChatPopup

--- src/Messenger.js
+++ src/Messenger.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import styles from './Messenger.css'
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect
-} from 'react-router-dom'
-import AllConversations from './AllConversations'
-import Sidebar from './Sidebar'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { setup } from './DataStore'
+import ChatPopup from './ChatPopup'
+import asyncCmp from './asyncComponent'
+
+const Messages = asyncCmp(() => import('./Messages').then(mod => mod.default))
 
 setup()
 
@@ -27,16 +23,8 @@ export default class Messenger extends React.Component {
     }
     return (
       <Router>
-        <div className={styles.MessengerContainer}>
-          <Sidebar />
-          <Switch>
-            <Route
-              path="/conversations/all/:conversationId?"
-              component={AllConversations}
-            />
-            <Redirect exact from="/conversations" to="/conversations/all" />
-          </Switch>
-        </div>
+        <Route path="/conversations" component={Messages} />
+        <ChatPopup />
       </Router>
     )
   }

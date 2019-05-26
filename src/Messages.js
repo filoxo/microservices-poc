dae2e794cd8134ac55c6749aec7210d35c6015ab
Carlos Filoteo <c.filoteo12@gmail.com>
2019-05-25 20:53:27 -0600

    Separate Messages from Messenger

---
+++ src/Messages.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from './Messages.css'
+import { Route, Switch, Redirect } from 'react-router-dom'
+import AllConversations from './AllConversations'
+import Sidebar from './Sidebar'
+import { setup } from './DataStore'
+
+setup()
+
+export default function Messages() {
+  return (
+    <div className={styles.MessengerContainer}>
+      <Sidebar />
+      <Switch>
+        <Route
+          path="/conversations/all/:conversationId?"
+          component={AllConversations}
+        />
+        <Redirect exact from="/conversations" to="/conversations/all" />
+      </Switch>
+    </div>
+  )
+}

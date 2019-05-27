fa7af50e4cde8a8321a1fd456c064f56306636fa
Carlos Filoteo <c.filoteo12@gmail.com>
2019-05-27 15:31:40 -0600

    Flesh out ChatPopup

--- src/ChatPopup.js
+++ src/ChatPopup.js
@@ -2,15 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import styles from './ChatPopup.css'
 import SelectedConversation from './SelectedConversation'
+import useConversationListener from './useConversationListener'
+import { ReactComponent as Close } from './icons/close-outline.svg'
+import IconButton from './IconButton'
 
 export default function ChatPopup() {
-  return ReactDOM.createPortal(
-    <div className={styles.ChatPopup}>
-      <div className={styles.Title}>Conversation</div>
-      <div className={styles.ConversationWrapper}>
-        <SelectedConversation conversationId="23456" />
-      </div>
-    </div>,
-    document.getElementById('chat-popup')
-  )
+  const conversationId = useConversationListener()
+
+  const closeConversation = () => {
+    var event = new CustomEvent('conversation:popup', {
+      detail: { conversationId: null }
+    })
+    document.dispatchEvent(event)
+  }
+
+  return conversationId
+    ? ReactDOM.createPortal(
+        <div className={styles.ChatPopup}>
+          <div className={styles.ConversationWrapper}>
+            <SelectedConversation
+              conversationId="23456"
+              toolbar={
+                <div className={styles.Title}>
+                  Conversation
+                  <IconButton
+                    onClick={closeConversation}
+                    label="Close conversation"
+                  >
+                    <Close width="16" />
+                  </IconButton>
+                </div>
+              }
+            />
+          </div>
+        </div>,
+        document.getElementById('chat-popup')
+      )
+    : null
 }
